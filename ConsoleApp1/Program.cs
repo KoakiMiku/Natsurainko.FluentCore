42b37bf5d7ad50c86de54a3c4f9d2be9232df6b3
KoakiMiku <2234975857@qq.com>
2023-10-09 21:37:36 +0800

    test

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json;
+using Nrk.FluentCore.Utils;
+
+try
+{
+    var memoryMetrics = MemoryUtils.GetWindowsMetrics();
+    Console.WriteLine(JsonSerializer.Serialize(memoryMetrics));
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
